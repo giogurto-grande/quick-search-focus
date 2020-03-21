bc3856f729b121297ee281e9f9d4c8f0424a2a9e
Kamil Kozłowski <kamil.kozlowski@vectorsynergy.com>
2020-03-21 20:25:08 +0100

    now the first visible input gets the focus - works for google

--- content.js
+++ content.js
@@ -1,16 +1,26 @@
-document.onkeypress = function (event) {
-    if (event.key === '/' && event.target.localName === 'body') {
-        const elements = document.getElementsByTagName('input');
+const focusOnSearchInput = function () {
+    const elements = document.getElementsByTagName('input');
 
-        for (let i in elements) {
-            if (!elements.hasOwnProperty(i)) {
-                break;
-            }
+    for (let i in elements) {
+        if (!elements.hasOwnProperty(i)) {
+            continue;
+        }
 
-            let element = elements[i];
+        let element = elements[i];
+        if(element.offsetParent !== null)
+        {
             element.focus();
-
             return false;
         }
     }
+
+    return true;
+};
+
+document.onkeypress = function (event) {
+    if (!(event.key === '/' && event.target.localName === 'body')) {
+        return true;
+    }
+
+    return focusOnSearchInput();
 };
